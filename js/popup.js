673e623c0a672e5ea6327fc4b4f9e1ef30626c78
kntsv <kuntsevych2020ki12@student.karazin.ua>
2023-09-06 23:08:04 +0300

    Update style and add adaptive for phones

---
+++ js/popup.js
@@ -0,0 +1,15 @@
+// function showPopupMessage(message, isSuccess) {
+//     var popupElement = document.getElementById("popup-message"); 
+//     popupElement.innerHTML = message; 
+
+//     if(isSuccess) {
+//         popupElement.style.background = "#4caf50";
+//     } else {
+//         popupElement.style.background = "ff6347"; 
+//     } 
+
+//     popupElement.style.display = "block"; 
+//     setTimeout(function() {
+//         popupElement.style.display = "none"; 
+//     }, 3000);
+// }
--- php/submit_resume.php
+++ php/submit_resume.php
@@ -1,23 +1,79 @@
 <?php
-if($_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $full_name = $_POST["full_name"];
-    $email = $_POST["email"]; 
+    $email = $_POST["email"];
+    $position = $_POST["position"]; 
     $phone = $_POST["phone"];
     $resume = $_POST["resume"];
+    $file = $_FILES["file"];
 
-    $to = "kuntesvish@gamil.com"; 
-    $subject = "Нове резюме";
-    $message = "ПІБ: $full_name\n Email: $email\n Телефон: $phone\n \n Резюме: \n$resume";  
-    
-    $headers = "From $email";
+    // Set recipient email address
+    $to = "offer@ltv.kiev.ua";
 
-    if(mail($to, $subject, $message, $headers)) {
-        echo "Резюме відправлено успішно";
-    } else {
-        echo "Помилка, резюме не відправлено";
+    // Set email subject
+    $subject = "Resume Submission from $full_name";
+
+    // Set email headers
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+
+    // Check if file is uploaded
+    if (isset($file) && $file["error"] == 0) {
+        // Get file info
+        $file_name = basename($file["name"]);
+        $file_type = $file["type"];
+        $file_size = filesize($file["tmp_name"]);
+
+        // Read file content into a variable
+        $handle = fopen($file["tmp_name"], "r");
+        $content = fread($handle, $file_size);
+        fclose($handle);
+
+        // Encode file content for sending in email
+        $content = chunk_split(base64_encode($content));
+
+        // Generate a boundary string
+        $semi_rand = md5(time());
+        $mime_boundary = "==Multipart_Boundary_x{$semi_rand}x";
+
+        // Add headers for file attachment// Add headers for text part of the message
+        $headers .= "MIME-Version: 1.0\r\n";
+        $headers .= "Content-Type: multipart/mixed; boundary=\"{$mime_boundary}\"\r\n";
+
+
+
+
+        // Add multipart boundary and file attachment to message body
+        // Add text part of the message
+        $body = "--{$mime_boundary}\r\n";
+        $body .= "Content-Type: text/plain; charset=\"UTF-8\"\r\n";
+        $body .= "Content-Transfer-Encoding: 7bit\r\n\r\n";
+        $body .= "Full Name: $full_name\n";
+        $body .= "Position: $position\n";
+        $body .= "Email: $email\n";
+        $body .= "Phone: $phone\n";
+        $body .= "Resume: $resume\n\r\n";
+        $body .= "--{$mime_boundary}\r\n";
+        $body .= "Content-Type: {$file_type};\r\n";
+        $body .= " name=\"{$file_name}\"\r\n";
+        $body .= "Content-Disposition: attachment;\r\n";
+        $body .= " filename=\"{$file_name}\"\r\n";
+        $body .= "Content-Transfer-Encoding: base64\r\n\r\n";
+        $body .= "{$content}\r\n\r\n";
+        $body .= "--{$mime_boundary}--\r\n";
     }
 
+
+    $flgchk = mail($to, $subject, $body, $headers); 
+    // $flgchk = true; 
+
+    // Send email
+    if ($flgchk) {
+        echo "<script language='javascript' type='text/javascript'>
+        showPopupMessage('$successMessage', true);
+        </script>";  
+    } else {
+        echo "<script language='javascript' type='text/javascript'>
+        showPopupMessage('$errorMessage', false);</script>";
+    }
 }
-
-
-?>
